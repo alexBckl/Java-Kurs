d83eb51d46adc629d8894c20cf2d41d3427dd14e
alexBckl <alexander.boeckle@gmail.com>
2017-04-06 15:16:59 +0200

    15:15

--- Spielesammlung/src/Main/Main.java
+++ Spielesammlung/src/Main/Main.java
@@ -7,12 +7,20 @@ public class Main {
 	public static void main(String[] args) {
 		Oberflaeche dieOberflaeche = new Oberflaeche();
 		Spielsteuerung dieSpielsteuerung;
+		
 		dieOberflaeche.gebeAus("Welches Spiel möchten Sie starten?" , true);
 		dieOberflaeche.gebeAus("1: TicTacToe          2: 4 Gewinnt", true);
 		dieOberflaeche.gebeAus("Eingabe: ", false);
 		
+		int auswahl = dieOberflaeche.leseZahl();
 		
-		if(dieOberflaeche.leseZahl() == 1) 
+		while(auswahl != 1 && auswahl != 0) {
+			dieOberflaeche.gebeAus("Fehlerhafte Eingabe!", true);
+			dieOberflaeche.gebeAus("Eingabe: ", false);
+			auswahl = dieOberflaeche.leseZahl();
+		}
+		
+		if(auswahl == 1) 
 			dieSpielsteuerung = new SteuerungTicTacToe(dieOberflaeche);
 		else
 			dieSpielsteuerung = new SteuerungVierGewinnt(dieOberflaeche);
