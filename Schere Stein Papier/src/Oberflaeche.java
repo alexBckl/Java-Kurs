0be652b30b51754028b798cf0d45957218713890
alexBckl <alexander.boeckle@gmail.com>
2017-04-18 11:07:23 +0200

    11:07

--- Schere Stein Papier/src/Oberflaeche.java
+++ Schere Stein Papier/src/Oberflaeche.java
@@ -1,4 +1,23 @@
+import java.util.Scanner;
 
 public class Oberflaeche {
-
+	//Attribute
+	private Scanner derScanner = new Scanner(System.in);
+	
+	public void gebeAus(String text, boolean zeilenumbruch) {
+		System.out.print(text);
+		if(zeilenumbruch) {
+			System.out.println();
+		}
+	}
+	
+	public int leseZahl(String text) {
+		System.out.print(text);
+		return derScanner.nextInt();
+	}
+	
+	public String leseText(String text) {
+		System.out.print(text);
+		return derScanner.nextLine();
+	}
 }
--- Schere Stein Papier/src/Spieler.java
+++ Schere Stein Papier/src/Spieler.java
@@ -1,4 +1,23 @@
 
 public class Spieler {
-
+	//Attribute
+	private String name;
+	private int punkte;
+	
+	//Operationen
+	public Spieler(String name) {
+		this.name = name;
+	}
+	
+	public String gebeName() {
+		return name;
+	}
+	
+	public int gebePunkte() {
+		return punkte;
+	}
+	
+	public void erhoehePunkte() {
+		punkte++;
+	}
 }
