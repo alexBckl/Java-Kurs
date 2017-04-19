8a0312d34347975adfbd9b38c978d6c81b51c2a1
alexBckl <alexander.boeckle@gmail.com>
2017-04-19 09:08:51 +0200

    9:08

--- Wiederholung/src/Array2D.java
+++ Wiederholung/src/Array2D.java
@@ -1,6 +1,12 @@
 
 public class Array2D {
 	public void Uebung() {
+		int[][] array2d = new int[2][10];
 		
+		for(int x = 0; x < 10; x++) {
+			array2d[0][x] = 2*x;
+			array2d[1][x] = 2*x + x*x;
+			System.out.println(x + " => [" + array2d[0][x] + "] [" + array2d[1][x] + "]" );
+		}
 	}
 }
