b939c83838cdb778228d0abd1085178351c078b4
alexBckl <alexander.boeckle@gmail.com>
2017-04-19 08:41:44 +0200

    Arrays 8:41

--- Wiederholung/src/Main.java
+++ Wiederholung/src/Main.java
@@ -1,7 +1,12 @@
 
 public class Main {
 	public static void main(String[] args) {
+		//Ternärer Operator
 		TernaryOperator derTernaryOperator = new TernaryOperator();
-		derTernaryOperator.Uebung();
+		//derTernaryOperator.Uebung();
+		
+		//Arrays
+		Array dasArray = new Array();
+		dasArray.Uebung();
 	}
 }
